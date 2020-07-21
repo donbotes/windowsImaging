f9cef60fcc2a028709d7a70dca41bee742ea9c0a
donbotes <68427652+donbotes@users.noreply.github.com>
2020-07-21 13:50:43 +0200

    Update testPsScript.ps1

--- testPsScript.ps1
+++ testPsScript.ps1
@@ -1,5 +1,5 @@
 mkdir c:\testingGithub
 echo Azure-Image-Builder-Was-Here  > c:\testingGithub\azureImageBuilder.txt
 
-mkdir c:\buildArtifacts
-echo Azure-Image-Builder-Was-Here  > c:\buildArtifacts\azureImageBuilder.txt
+New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
+
