f49629d0c982a4bb48bad8e0949e0e51cd95c522
donbotes <68427652+donbotes@users.noreply.github.com>
2020-07-22 14:24:38 +0200

    FXLogix installer

---
+++ FXLogix.ps1
@@ -0,0 +1,10 @@
+﻿#Create temp folder
+New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
+
+#InstallFSLogix
+write-host "Installing FSLogix"
+Invoke-WebRequest -Uri 'https://aka.ms/fslogix_download' -OutFile 'c:\temp\fslogix.zip'
+Start-Sleep -Seconds 10
+Expand-Archive -Path 'C:\temp\fslogix.zip' -DestinationPath 'C:\temp\fslogix\'  -Force
+C:\temp\fslogix\x64\Release\FSLogixAppsSetup.exe /install /quiet /norestart
+Start-Sleep -Seconds 120
