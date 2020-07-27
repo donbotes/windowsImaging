4e788c86104d264c3161b58f9416c704aead70ab
donbotes <68427652+donbotes@users.noreply.github.com>
2020-07-27 16:19:18 +0200

    Update Installations1.ps1

--- Installations1.ps1
+++ Installations1.ps1
@@ -1,16 +1,6 @@
 ﻿#Create temp folder
 New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
 
-Logfile = "C:\temp\powershell.log"
-
-Function LogWrite
-{
-   Param ([string]$logstring)
-
-   Add-content $Logfile -value $logstring
-}
-
-
 #Chrome
 write-host "Installing Chrome"
 Invoke-WebRequest -Uri 'https://dl.google.com/tag/s/appguid%253D%257B8A69D345-D564-463C-AFF1-A69D9E530F96%257D%2526iid%253D%257BBEF3DB5A-5C0B-4098-B932-87EC614379B7%257D%2526lang%253Den%2526browser%253D4%2526usagestats%253D1%2526appname%253DGoogle%252520Chrome%2526needsadmin%253Dtrue%2526ap%253Dx64-stable-statsdef_1%2526brand%253DGCEB/dl/chrome/install/GoogleChromeEnterpriseBundle64.zip?_ga%3D2.8891187.708273100.1528207374-1188218225.1527264447' -OutFile 'c:\temp\chrome.zip'
