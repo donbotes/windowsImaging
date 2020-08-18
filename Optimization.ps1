735fa09022c5116e9c89e19b2e88c83776ee69c9
donbotes <68427652+donbotes@users.noreply.github.com>
2020-08-18 12:53:17 +0200

    To download and execute Optimization.zip

---
+++ Optimization.ps1
@@ -0,0 +1,18 @@
+﻿#Create temp folder
+New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
+
+Start-Transcript `
+-Path 'C:\temp\powershell-installation.log' `
+-IncludeInvocationHeader
+
+
+#Optimizations
+write-host "Downloading Optimizations"(Get-Date)
+Invoke-WebRequest -Uri 'https://github.com/donbotes/windowsImaging/raw/master/Optimizations.zip' -OutFile 'c:\temp\Optimizations.zip'
+write-host "Download Done"(Get-Date)
+write-host "Extracting Optimizations"(Get-Date)
+Expand-Archive -Path 'C:\temp\Optimizations.zip' -DestinationPath 'C:\temp\Optimizations\'  -Force
+write-host "Extract Done"(Get-Date)
+write-host "Optimizing..."(Get-Date)
+Invoke-Expression -Command 'C:\temp\Optimizations\Virtual-Desktop-Optimization-Tool-master\Win10_VirtualDesktop_Optimize.ps1' 
+Stop-Transcript
