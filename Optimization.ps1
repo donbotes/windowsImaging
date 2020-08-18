9ada9ef28cb4af388c3203a41870b581259a076d
donbotes <68427652+donbotes@users.noreply.github.com>
2020-08-18 16:16:21 +0200

    Update Optimization.ps1

--- Optimization.ps1
+++ Optimization.ps1
@@ -11,7 +11,7 @@ write-host "Downloading Optimizations"(Get-Date)
 Invoke-WebRequest -Uri 'https://github.com/donbotes/windowsImaging/raw/master/Optimizations.zip' -OutFile 'c:\temp\Optimizations.zip'
 write-host "Download Done"(Get-Date)
 write-host "Extracting Optimizations"(Get-Date)
-Expand-Archive -Path 'C:\temp\Optimizations.zip' -DestinationPath 'C:\temp\Optimizations\'  -Force
+Expand-Archive -Path 'C:\temp\Optimizations.zip' -DestinationPath 'C:\temp\' -Force
 write-host "Extract Done"(Get-Date)
 write-host "Optimizing..."(Get-Date)
 Invoke-Expression -Command 'C:\temp\Optimizations\Virtual-Desktop-Optimization-Tool-master\Win10_VirtualDesktop_Optimize.ps1' 
