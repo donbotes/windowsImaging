4bd82d0cbb4f3bb90f792a6f79456e9fe1050fef
donbotes <68427652+donbotes@users.noreply.github.com>
2020-09-01 09:11:14 +0200

    Update Teams.ps1

--- Teams.ps1
+++ Teams.ps1
@@ -10,8 +10,9 @@ write-host "Downloading Datadog"(Get-Date)
 Invoke-WebRequest -Uri 'https://statics.teams.cdn.office.net/production-windows-x64/1.3.00.21759/Teams_windows_x64.msi' -OutFile 'c:\temp\teams.msi'
 write-host "Download Done"(Get-Date)
 write-host "Installing Datadog"(Get-Date)
-msiexec /i 'c:\temp\teams.msi' /l*v 'C:\temp\teams-install.log' ALLUSER=1 ALLUSERS=1
-#write-host "Waiting the 120 seconds"(Get-Date)
-#Start-Sleep -Seconds 120
+#msiexec /i 'c:\temp\teams.msi' /l*v 'C:\temp\teams-install.log' ALLUSER=1 ALLUSERS=1
+Start-Process -Wait msiexec -ArgumentList '/qn /i c:\temp\teams.msi' ALLUSER=1 ALLUSERS=1
+write-host "Waiting the 120 seconds"(Get-Date)
+Start-Sleep -Seconds 120
 write-host "Datadog Done"(Get-Date)
 Stop-Transcript
